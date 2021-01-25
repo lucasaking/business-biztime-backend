a08914a64d4c62ab3655b36ebb70625bcdf8f08a
lucasaking <70596715+lucasaking@users.noreply.github.com>
2021-01-25 02:41:15 -0500

    updating repo

---
+++ routes/companies.js
@@ -0,0 +1,151 @@
+/** Routes for companies. */
+
+const express = require("express");
+const slugify = require("slugify");
+const { NotFoundError } = require("../expressError");
+
+const db = require("../db");
+
+let router = new express.Router();
+
+
+/** GET / => list of companies.
+ *
+ * =>  {companies: [{code, name, descrip}, {code, name, descrip}, ...]}
+ *
+ * */
+
+router.get("/", async function (req, res, next) {
+  try {
+    const results = await db.query(`
+      SELECT code, name
+      FROM companies
+      ORDER BY name`,
+    );
+    const companies = results.rows;
+
+    return res.json({ companies });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+/** GET /[code] => detail on company
+ *
+ * =>  {company: {code, name, descrip, invoices: [id, ...]}}
+ *
+ * */
+
+router.get("/:code", async function (req, res, next) {
+  try {
+    let code = req.params.code;
+
+    const compResults = await db.query(`
+          SELECT code, name, description
+          FROM companies
+          WHERE code = $1`,
+        [code],
+    );
+    const company = compResults.rows[0];
+
+    if (!company) throw new NotFoundError(`No such company: ${code}`);
+
+    const invResults = await db.query(`
+          SELECT id
+          FROM invoices
+          WHERE comp_code = $1`,
+        [code],
+    );
+    const invoices = invResults.rows;
+
+    company.invoices = invoices.map(inv => inv.id);
+    return res.json({ "company": company });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+/** POST / => add new company
+ *
+ * {name, descrip}  =>  {company: {code, name, descrip}}
+ *
+ * */
+
+router.post("/", async function (req, res, next) {
+  try {
+    let { name, description } = req.body;
+    let code = slugify(name, { lower: true });
+
+    const results = await db.query(
+          `INSERT INTO companies (code, name, description)
+           VALUES ($1, $2, $3)
+           RETURNING code, name, description`,
+        [code, name, description]);
+    const company = results.rows[0];
+
+    return res.status(201).json({ company });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+/** PUT /[code] => update company
+ *
+ * {name, descrip}  =>  {company: {code, name, descrip}}
+ *
+ * */
+
+router.put("/:code", async function (req, res, next) {
+  try {
+    let { name, description } = req.body;
+    let code = req.params.code;
+
+    const results = await db.query(
+          `UPDATE companies
+           SET name=$1,
+               description=$2
+           WHERE code = $3
+           RETURNING code, name, description`,
+        [name, description, code]);
+    const company = results.rows[0];
+
+    if (!company) throw new NotFoundError(`No such company: ${code}`);
+
+    return res.json({ company });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+/** DELETE /[code] => delete company
+ *
+ * => {status: "added"}
+ *
+ */
+
+router.delete("/:code", async function (req, res, next) {
+  try {
+    let code = req.params.code;
+
+    const results = await db.query(
+          `DELETE
+           FROM companies
+           WHERE code = $1
+           RETURNING code`,
+        [code]);
+    const company = results.rows[0];
+
+    if (!company) throw new NotFoundError(`No such company: ${code}`);
+
+    return res.json({ "status": "deleted" });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+module.exports = router;
